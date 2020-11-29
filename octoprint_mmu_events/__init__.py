f1e410e5b6f0a81002b8ce5e32840abe3c96b516
Chris Speck <cgspeck@gmail.com>
2020-11-29 11:26:14 +1100

    Expose an enabled switch

--- octoprint_mmu_events/__init__.py
+++ octoprint_mmu_events/__init__.py
@@ -25,8 +25,14 @@ class Mmu_eventsPlugin(octoprint.plugin.SettingsPlugin,
 
     def get_settings_defaults(self):
         return dict(
-            # put your plugin's default settings here
+            enabled=True
         )
+
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False),
+            dict(type="settings", custom_bindings=False)
+        ]
 
     ##~~ AssetPlugin mixin
 
@@ -65,7 +71,7 @@ class Mmu_eventsPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Mmu_events Plugin"
+__plugin_name__ = "MMU Events"
 
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
