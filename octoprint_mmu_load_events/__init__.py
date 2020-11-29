f1838381284f222eb8ed705adb2a1bc789ccc027
Chris Speck <cgspeck@gmail.com>
2020-11-29 15:25:59 +1100

    Remove unnecessary files

--- octoprint_mmu_load_events/__init__.py
+++ octoprint_mmu_load_events/__init__.py
@@ -2,21 +2,14 @@
 from __future__ import absolute_import, unicode_literals
 import logging
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 from octoprint.events import eventManager
 
+
+__plugin_name__ = "MMU Load Events"
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+
 class Mmu_load_eventsPlugin(
-    octoprint.plugin.SettingsPlugin,
-    octoprint.plugin.AssetPlugin,
-    octoprint.plugin.TemplatePlugin,
     octoprint.plugin.StartupPlugin,
     octoprint.plugin.EventHandlerPlugin
 ):
@@ -55,47 +48,16 @@ class Mmu_load_eventsPlugin(
         # PLUGIN_MMU_LOAD_EVENTS_SUCCESS, PLUGIN_MMU_LOAD_EVENTS_FAILED, PLUGIN_MMU_LOAD_EVENTS_UNKNOWN
         return ["success", "failed", "unknown"]
 
-    ##~~ EventHandlerPlugin
-    def on_event(self, event, payload):
-        pass
-
     ##~~ StartupPlugin
     def on_after_startup(self):
         self._logger.info("MMU Load Events plugin saying g'day!")
 
-    ##~~ SettingsPlugin mixin
-
-    def get_settings_defaults(self):
-        return dict(
-            enabled=True
-        )
-
-    def get_template_configs(self):
-        return [
-            dict(type="navbar", custom_bindings=False),
-            dict(type="settings", custom_bindings=False)
-        ]
-
-    ##~~ AssetPlugin mixin
-
-    def get_assets(self):
-        # Define your plugin's asset files to automatically include in the
-        # core UI here.
-        return dict(
-            js=["js/mmu_load_events.js"],
-            css=["css/mmu_load_events.css"],
-            less=["less/mmu_load_events.less"]
-        )
-
     ##~~ Softwareupdate hook
-
     def get_update_information(self):
-        # Define the configuration for your plugin to use with the Software Update
-        # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
-        # for details.
+        # See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
         return dict(
             mmu_load_events=dict(
-                displayName="Mmu_load_events Plugin",
+                displayName=__plugin_name__,
                 displayVersion=self._plugin_version,
 
                 # version check: github repository
@@ -108,20 +70,6 @@ class Mmu_load_eventsPlugin(
                 pip="https://github.com/cgspeck/OctoPrint-Mmu_load_events/archive/{target_version}.zip"
             )
         )
-
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "MMU Load Events"
-
-# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
-# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
-# compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
-
 
 def __plugin_load__():
     global __plugin_implementation__
